167076f9af40958dd02525dd71d6dd48b03e5908
prasangr <53392337+prasangr@users.noreply.github.com>
2025-01-24 10:19:43 +0530

    new project , added the basic files

---
+++ EcommerceApplication/src/main/java/com/example/EcommerceApplication/Controller/Controller.java
@@ -0,0 +1,15 @@
+package com.example.EcommerceApplication.Controller;
+
+import org.springframework.aot.hint.annotation.Reflective;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+@RequestMapping("/hello")
+public String hello() {
+return "Hello World";
+}
+
+}
